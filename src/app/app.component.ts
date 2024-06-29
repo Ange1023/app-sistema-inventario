3126bdd0caca6a31b237d29002a270ed7234249a
Idiar <116418972+idiarj@users.noreply.github.com>
2024-06-29 04:04:58 -0400

    ayuda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
+  isLoggedIn: string = 'false';
+  private authSubscription: Subscription;
+
+  constructor(private authService: AuthService) {
+    this.authSubscription = this.authService.getLoggedInStatus().subscribe(isLoggedIn => {
+      this.isLoggedIn = isLoggedIn ? 'true' : 'false';
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.authSubscription) {
+      this.authSubscription.unsubscribe();
+    }
+  }
+
   title = 'proyecto-inventario';
 }
