30db71f7fdb3d8515f705b419a513b068abf2db5
Idiarj <idiar16@gmail.com>
2024-07-05 23:35:57 -0400

    backend 1

---
+++ backend/sub-sistemas/DBComp.js
@@ -0,0 +1,39 @@
+import { Pool } from 'pg'
+
+export class DBComp{
+    constructor(config){
+        this.config = config;
+        this.Pool = new Pool(this.config);
+    }
+
+    async getConnection(){
+        try{
+            const client = await this.Pool.connect();
+            return client;
+        }catch(err){
+            console.log(err);
+            return null;
+        }
+    }
+
+    async exeQuery({query, param = []}){
+        const client = await this.getConnection();
+        try {
+            const {rows} = await client.query(query, param);
+            return rows;
+        } catch (error) {
+            console.log(error);
+            return null;
+        }finally{
+            await this.closeConnection(client);
+        }
+    }
+
+    async closeConnection(client){
+        try{
+            await client.release();
+        }catch(err){
+            console.log(err);
+        }
+    }
+}
